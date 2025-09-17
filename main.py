55d6b76542dcaa839fb055a62c6e31a43d815ac7
Rajat Khandelwal <rajatkhandelwal191@gmail.com>
2025-09-17 17:47:48 +0530

    custom prompt chain invoke

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from langchain.chains.retrieval import create_retrieval_chain
 from langchain_pinecone import PineconeVectorStore
 from langchain_pinecone import PineconeEmbeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain
-
-
+from langchain_core.runnables import RunnablePassthrough
 
 
 load_dotenv()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 if __name__ == "__main__":
     print("retrieving")
@@ -46,8 +48,29 @@ if __name__ == "__main__":
 
     result = retrival_chain.invoke(input={"input": query})
 
-    print(result)
+    # print(result)
 
+    template = """Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+    Always say "thanks for asking!" at the end of the answer.
+
+    {context}
+
+    Question: {question}
+
+    Helpful Answer."""
+
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+        {
+            "context":vectorstore.as_retriever() | format_docs , "question": RunnablePassthrough()
+        } | custom_rag_prompt | llm
+    )
+
+    res = rag_chain.invoke(query)
+    print(res)
 
 
 
