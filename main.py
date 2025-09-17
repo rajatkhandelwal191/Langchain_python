9aeab079853b1842b9ddea55fbecc2c24f879986
Rajat Khandelwal <rajatkhandelwal191@gmail.com>
2025-09-17 18:57:31 +0530

    google ai embedding and faiss vectorstore intiate for local vector store

---
+++ main.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+from langchain_community.vectorstores import FAISS
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain import hub
+# from langchain_pinecone import PineconeVectorStore
+# from langchain_pinecone import PineconeEmbeddings
+
+
+if __name__ == "__main__":
+    print("hi")
+    pdf_path = "react.pdf"
+    loader = PyPDFLoader(file_path=pdf_path)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=30, separator="\n"
+    )
+    docs = text_splitter.split_documents(documents=documents)
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+    
+    vectorstore = FAISS.from_documents(docs, embeddings)
+    vectorstore.save_local("faiss_index_react")
+
+    new_vectorstore = FAISS.load_local(
+        "faiss_index_react", embeddings, allow_dangerous_deserialization=True
+    )
+
+    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(
+        llm, retrieval_qa_chat_prompt
+    )
+
+    retrieval_chain = create_retrieval_chain(
+        new_vectorstore.as_retriever(), combine_docs_chain
+    )
+
+    res = retrieval_chain.invoke({"input": "Give me the gist of ReAct in 3 sentences"})
+    print(res["answer"])
+
