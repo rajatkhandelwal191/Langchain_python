87c1f47999d0af57f666875b70f98f9a12d35f1b
Rajat Khandelwal <rajatkhandelwal191@gmail.com>
2025-09-20 09:29:56 +0530

    all docs - crawl and take the object result comes

--- ingestion.py
+++ ingestion.py
@@ -9,7 +9,8 @@ from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma # local vectorstore like faiss
 from langchain_core.documents import Document
-from langchain_openai import OpenAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 from langchain_pinecone import PineconeVectorStore, PineconeEmbeddings
 from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
 
@@ -28,8 +29,8 @@ os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 embeddings = PineconeEmbeddings(
         model="llama-text-embed-v2",
         pinecone_api_key=os.environ.get("PINECONE_API_KEY"),
-        batch_size=50,
-        retry_min_seconds=10
+        # batch_size=50,
+        # retry_min_seconds=10
     )
 
 
@@ -38,3 +39,27 @@ tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
 tavily_crawl = TavilyCrawl()
 
 
+async def main():
+    """Main async function to orchestrate the entire process"""
+    log_header("Documentation Ingestion Pipeline")
+    log_info(
+    "🔍 TavilyCrawl: Starting to crawl documentation from https://python.langchain.com/",
+    Colors.PURPLE,
+    )
+
+    res = tavily_crawl.invoke(
+        {
+            "url": "https://python.langchain.com/",
+            "extract_depth": "advanced",
+            "instructions": "Documentatin relevant to ai agents",
+            "max_depth": 1,
+        }
+    )
+
+    # all_docs = res["results"]
+    all_docs = [Document(page_content=result['raw_content'], metadata={"source":result['url']}) for result in res['result']]
+    log_success(f"TavilyCrawl: Successfully crawled {len(all_docs)} URLs from documentation site")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
